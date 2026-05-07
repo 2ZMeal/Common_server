0a92162bbf8a61a6c3dfff7706d6d58325955027
hjk2132 <xiest@naver.com>
2026-05-07 12:11:07 +0900

    refactor: X-Trace-Id 삭제 및 의존성 추가

--- src/main/java/com/ezmeal/common/security/interceptor/FeignClientInterceptor.java
+++ src/main/java/com/ezmeal/common/security/interceptor/FeignClientInterceptor.java
@@ -3,7 +3,6 @@ package com.ezmeal.common.security.interceptor;
 import com.ezmeal.common.security.principal.CustomUserPrincipal;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import org.slf4j.MDC;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -13,11 +12,6 @@ public class FeignClientInterceptor implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate template) {
-        // MDC에서 traceId를 가져와서 Http 헤더에 추가
-        String traceId = MDC.get("traceId");
-        if (traceId != null) {
-            template.header("X-TraceId", traceId);
-        }
 
         // 현재 스레드의 SecurityContext에서 유저 정보를 꺼내서 HTTP 헤더에 담기
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
