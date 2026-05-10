03cfbd88541c935dbb3c0c5f113823a80d3aaf33
hjk2132 <xiest@naver.com>
2026-05-10 21:47:07 +0900

    fix: DomainEvent 인터페이스에 구현 클래스 정보를 알 수 있도록 수정

--- src/main/java/com/ezmeal/common/message/DomainEvent.java
+++ src/main/java/com/ezmeal/common/message/DomainEvent.java
@@ -5,5 +5,9 @@ package com.ezmeal.common.message;
     실제 양식은 EventEnvelop와 CommonKafkaEventPublisher를 통함
 */
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+// 인터페이스를 구현한 객체가 직렬화될 때 구현한 객체의 클래스(implements한 클래스의 정보를) 알 수 있도록 어노테이션 추가
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@class")
 public interface DomainEvent {
 }
